265767f1f808808d4a27583ea7177c7140df245d
Dennis <337991+statler@users.noreply.github.com>
2019-12-16 17:32:06 +1100

    Misc commit of stuff

--- src/NJsonSchema.CodeGeneration.TypeScript/RenamePropertyFactory.cs
+++ src/NJsonSchema.CodeGeneration.TypeScript/RenamePropertyFactory.cs
@@ -23,7 +23,7 @@ namespace NJsonSchema.CodeGeneration.TypeScript
             else return PropertyName;
         }
 
-        static Dictionary<string, Dictionary<string, string>> dctRenames;
+        static Dictionary<string, Dictionary<string, string>> _dctRenames;
         /// <summary>
         /// 
         /// </summary>
@@ -31,137 +31,136 @@ namespace NJsonSchema.CodeGeneration.TypeScript
         {
             get
             {
-                if (dctRenames == null) populateRenames();
-                return dctRenames;
+                if (_dctRenames == null) populateRenames();
+                return _dctRenames;
             }
         }
 
         static void populateRenames()
         {
-            dctRenames = new Dictionary<string, Dictionary<string, string>>();
-            dctRenames["All"] = new Dictionary<string, string>()
-            {
-                {"LotItpdetailId", "LotItpDetailId"},
-                {"LotItpid", "LotItpId"},
-                {"Itpid", "ItpId"},
-                {"Ncrid", "NcrId"},
-                {"Ncrno", "NcrNo"},
-                {"IsAvloverride", "IsAvlOverride"}
-            };
-            dctRenames["LotItpdetailViewModel"] = new Dictionary<string, string>()
-            {
-                {"Ncr", "NcrId"},
-                {"VerifyBy", "VerifyById"},
-                {"CheckedBy", "CheckedById"},
-                {"ApprovedBy", "ApprovedById"},
-                {"NcrNavigation", "Ncr"},
-                {"VerifyByNavigation", "VerifyByUser"},
-                {"CheckedByNavigation", "CheckedByUser"},
-                {"ApprovedByNavigation", "ApprovedByUser"}
-            };
-            dctRenames["LotItpViewModel"] = new Dictionary<string, string>()
-            {
-                {"VerifiedBy", "VerifiedById"},
-                {"CheckedBy", "CheckedById"},
-                {"ApprovedBy", "ApprovedById"},
-                {"VerifiedByNavigation", "VerifiedByUser"},
-                {"CheckedByNavigation", "CheckedByUser"},
-                {"ApprovedByNavigation", "ApprovedByUser"}
-            };
-            dctRenames["LotViewModel"] = new Dictionary<string, string>()
-            {
-                {"RaisedByNavigation", "RaisedByUser"},
-                {"ConformedByNavigation", "ConformedByUser"},
-                {"PrimaryTag", "PrimaryTagId"},
-                {"ControlLine", "ControlLineId"},
-                {"RaisedBy", "RaisedById"},
-                {"ConformedBy", "ConformedById"}
-            };
-            dctRenames["NcrViewModel"] = new Dictionary<string, string>()
-            {
-                {"RaisedByNavigation", "RaisedByUser"},
-                {"ApprovalByNavigation", "ApprovalByUser"},
-                {"CloseOutByNavigation", "CloseOutByUser"},
-                {"RaisedBy", "RaisedById"},
-                {"CloseOutBy", "CloseOutById"},
-                {"ApprovalBy", "ApprovalById"}
-            };
-            dctRenames["TestRequestViewModel"] = new Dictionary<string, string>()
-            {
-                {"RequestedByNavigation", "RequestedByUser"},
-                {"TestRequestToNavigation", "TestRequestToUser"},
-                {"ControlLine", "ControlLineId"},
-                {"RequestedBy", "RequestedById"},
-                {"TestRequestTo", "TestRequestToId"},
-                {"Daterequired", "DateRequired"},
-                {"DepthtoTest", "DepthToTest"}
-            };
-            dctRenames["TestCoordinateViewModel"] = new Dictionary<string, string>()
-            {
-                {"Trid", "TestRequestId"},
-                {"Tr", "TestRequest"}
-            };
-            dctRenames["ApprovalViewModel"] = new Dictionary<string, string>()
-            {
-                {"Ncrlink", "NcrLinkId"},
-                {"LotItpdetailLink", "LotItpDetailLinkId"},
-                {"LotQtyLink", "LotQtyLinkId"},
-                {"LotAtplink", "LotAtpLinkId"},
-                {"PurchaseOrderLink", "PurchaseOrderLinkId"},
-                {"PurchaseOrderAcceptanceLink", "PurchaseOrderAcceptanceLinkId"},
-                {"CloseOutNcr", "CloseOutNcrId"},
-                {"CloseOutBy", "CloseOutById"},
-                {"ResponseBy", "ResponseById"},
-                {"CloseOutByNavigation", "CloseOutByUser"},
-                {"ResponseByNavigation", "ResponseByUser"}
-            };
-            dctRenames["ApprovalRequestToViewModel"] = new Dictionary<string, string>()
-            {
-                {"RequestTo", "RequestToId"},
-                {"CctoEmail", "CcToEmail"},
-                {"RequestToNavigation", "RequestToUser"}
-            };
-            dctRenames["ApprovalRequestCcViewModel"] = new Dictionary<string, string>()
-            {
-                {"Ccto", "CctoId"},
-                {"CctoNavigation", "CcToUser"}
-            };
-            dctRenames["PurchaseOrderViewModel"] = new Dictionary<string, string>()
-            {
-                {"Poindex", "PoIndex"},
-                {"Ponumber", "PoNumber"},
-                {"Podate", "PoDate"},
-                {"Pocontact", "PoContactId"},
-                {"RaisedBy", "RaisedById"},
-                {"ApprovedBy", "ApprovedById"},
-                {"Pobudget", "PoBudget"},
-                {"Supplier", "SupplierId"},
-                {"SupplierConfirmationBy", "SupplierConfirmationById"},
-                {"ApprovedByNavigation", "ApprovedByUser"},
-                {"PocontactNavigation", "PoContactUsern"},
-                {"RaisedByNavigation", "RaisedByUser"},
-                {"SupplierConfirmationByNavigation", "SupplierConfirmationByUser"}
-            };
-            dctRenames["PurchaseOrderDtoViewModel"] = new Dictionary<string, string>()
-            {
-                {"Poindex", "PoIndex"},
-                {"Ponumber", "PoNumber"},
-                {"Podate", "PoDate"},
-                {"Pocontact", "PoContactId"},
-                {"RaisedBy", "RaisedById"},
-                {"ApprovedBy", "ApprovedById"},
-                {"Pobudget", "PoBudget"},
-                {"Supplier", "SupplierId"},
-                {"SupplierConfirmationBy", "SupplierConfirmationById"},
-                {"ApprovedByNavigation", "ApprovedByUser"},
-                {"PocontactNavigation", "PoContactUsern"},
-                {"RaisedByNavigation", "RaisedByUser"},
-                {"SupplierConfirmationByNavigation", "SupplierConfirmationByUser"}
-            };
-            dctRenames["PurchaseOrderDetailViewModel"] = new Dictionary<string, string>()
-            {
-                {"PodetailId", "PoDetailId"},
-            };
+            _dctRenames = new Dictionary<string, Dictionary<string, string>>();
+            //dctRenames["All"] = new Dictionary<string, string>()
+            //{
+            //    {"LotItpdetailId", "LotItpDetailId"},
+            //    {"LotItpid", "LotItpId"},
+            //    {"Itpid", "ItpId"},
+            //    {"Ncrid", "NcrId"},
+            //    {"Ncrno", "NcrNo"},
+            //    {"IsAvloverride", "IsAvlOverride"}
+            //};
+            //dctRenames["LotItpdetailViewModel"] = new Dictionary<string, string>()
+            //{
+            //    {"Ncr", "NcrId"},
+            //    {"VerifyBy", "VerifyById"},
+            //    {"CheckedBy", "CheckedById"},
+            //    {"ApprovedBy", "ApprovedById"},
+            //    {"VerifyByNavigation", "VerifyByUser"},
+            //    {"CheckedByNavigation", "CheckedByUser"},
+            //    {"ApprovedByNavigation", "ApprovedByUser"}
+            //};
+            //dctRenames["LotItpDetailDtoViewModel"] = dctRenames["LotItpdetailViewModel"];
+            //dctRenames["LotItpViewModel"] = new Dictionary<string, string>()
+            //{
+            //    {"VerifiedBy", "VerifiedById"},
+            //    {"CheckedBy", "CheckedById"},
+            //    {"ApprovedBy", "ApprovedById"},
+            //    {"VerifiedByNavigation", "VerifiedByUser"},
+            //    {"CheckedByNavigation", "CheckedByUser"},
+            //    {"ApprovedByNavigation", "ApprovedByUser"}
+            //};
+            //dctRenames["LotViewModel"] = new Dictionary<string, string>()
+            //{
+            //    {"RaisedByNavigation", "RaisedByUser"},
+            //    {"ConformedByNavigation", "ConformedByUser"},
+            //    {"PrimaryTag", "PrimaryTagId"},
+            //    {"ControlLine", "ControlLineId"},
+            //    {"RaisedBy", "RaisedById"},
+            //    {"ConformedBy", "ConformedById"}
+            //};
+            //dctRenames["NcrViewModel"] = new Dictionary<string, string>()
+            //{
+            //    {"RaisedByNavigation", "RaisedByUser"},
+            //    {"ApprovalByNavigation", "ApprovalByUser"},
+            //    {"CloseOutByNavigation", "CloseOutByUser"},
+            //    {"RaisedBy", "RaisedById"},
+            //    {"CloseOutBy", "CloseOutById"},
+            //    {"ApprovalBy", "ApprovalById"}
+            //};
+            //dctRenames["TestRequestViewModel"] = new Dictionary<string, string>()
+            //{
+            //    {"RequestedByNavigation", "RequestedByUser"},
+            //    {"TestRequestToNavigation", "TestRequestToUser"},
+            //    {"ControlLine", "ControlLineId"},
+            //    {"RequestedBy", "RequestedById"},
+            //    {"TestRequestTo", "TestRequestToId"},
+            //    {"Daterequired", "DateRequired"},
+            //    {"DepthtoTest", "DepthToTest"}
+            //};
+            //dctRenames["TestCoordinateViewModel"] = new Dictionary<string, string>()
+            //{
+            //    {"Trid", "TestRequestId"},
+            //    {"Tr", "TestRequest"}
+            //};
+            //dctRenames["ApprovalViewModel"] = new Dictionary<string, string>()
+            //{
+            //    {"Ncrlink", "NcrLinkId"},
+            //    {"LotItpdetailLink", "LotItpDetailLinkId"},
+            //    {"LotQtyLink", "LotQtyLinkId"},
+            //    {"LotAtplink", "LotAtpLinkId"},
+            //    {"PurchaseOrderLink", "PurchaseOrderLinkId"},
+            //    {"PurchaseOrderAcceptanceLink", "PurchaseOrderAcceptanceLinkId"},
+            //    {"CloseOutNcr", "CloseOutNcrId"},
+            //    {"CloseOutBy", "CloseOutById"},
+            //    {"ResponseBy", "ResponseById"},
+            //    {"CloseOutByNavigation", "CloseOutByUser"},
+            //    {"ResponseByNavigation", "ResponseByUser"}
+            //};
+            //_dctRenames["ApprovalRequestToViewModel"] = new Dictionary<string, string>()
+            //{
+            //    {"RequestTo", "RequestToId"},
+            //    {"RequestToNavigation", "RequestToUser"}
+            //};
+            //_dctRenames["ApprovalRequestToDtoViewModel"] = new Dictionary<string, string>()
+            //{
+            //    {"RequestTo", "RequestToId"},
+            //    {"RequestToNavigation", "RequestToUser"}
+            //};
+            //_dctRenames["PurchaseOrderViewModel"] = new Dictionary<string, string>()
+            //{
+            //    {"Poindex", "PoIndex"},
+            //    {"Ponumber", "PoNumber"},
+            //    {"Podate", "PoDate"},
+            //    {"Pocontact", "PoContactId"},
+            //    {"RaisedBy", "RaisedById"},
+            //    {"ApprovedBy", "ApprovedById"},
+            //    {"Pobudget", "PoBudget"},
+            //    {"Supplier", "SupplierId"},
+            //    {"SupplierConfirmationBy", "SupplierConfirmationById"},
+            //    {"ApprovedByNavigation", "ApprovedByUser"},
+            //    {"PocontactNavigation", "PoContactUsern"},
+            //    {"RaisedByNavigation", "RaisedByUser"},
+            //    {"SupplierConfirmationByNavigation", "SupplierConfirmationByUser"}
+            //};
+            //_dctRenames["PurchaseOrderDtoViewModel"] = new Dictionary<string, string>()
+            //{
+            //    {"Poindex", "PoIndex"},
+            //    {"Ponumber", "PoNumber"},
+            //    {"Podate", "PoDate"},
+            //    {"Pocontact", "PoContactId"},
+            //    {"RaisedBy", "RaisedById"},
+            //    {"ApprovedBy", "ApprovedById"},
+            //    {"Pobudget", "PoBudget"},
+            //    {"Supplier", "SupplierId"},
+            //    {"SupplierConfirmationBy", "SupplierConfirmationById"},
+            //    {"ApprovedByNavigation", "ApprovedByUser"},
+            //    {"PocontactNavigation", "PoContactUsern"},
+            //    {"RaisedByNavigation", "RaisedByUser"},
+            //    {"SupplierConfirmationByNavigation", "SupplierConfirmationByUser"}
+            //};
+            //_dctRenames["PurchaseOrderDetailViewModel"] = new Dictionary<string, string>()
+            //{
+            //    {"PodetailId", "PoDetailId"},
+            //};
         }
 
     }
